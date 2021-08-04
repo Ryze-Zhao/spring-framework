37f10472a17ba4bffefb49f96a8412183ea3f0fa
RyzeZhao <643080112@qq.com>
2021-08-04 21:57:57 +0800

    Spring::中文注释

--- spring-web/src/main/java/org/springframework/web/context/ConfigurableWebApplicationContext.java
+++ spring-web/src/main/java/org/springframework/web/context/ConfigurableWebApplicationContext.java
@@ -42,11 +42,13 @@ public interface ConfigurableWebApplicationContext extends WebApplicationContext
 
 	/**
 	 * Prefix for ApplicationContext ids that refer to context path and/or servlet name.
+	 * 引用上下文路径和/或servlet名称的ApplicationContext ID的前缀。
 	 */
 	String APPLICATION_CONTEXT_ID_PREFIX = WebApplicationContext.class.getName() + ":";
 
 	/**
 	 * Name of the ServletConfig environment bean in the factory.
+	 * 工厂中ServletConfig环境bean的名称。
 	 * @see javax.servlet.ServletConfig
 	 */
 	String SERVLET_CONFIG_BEAN_NAME = "servletConfig";
@@ -54,8 +56,9 @@ public interface ConfigurableWebApplicationContext extends WebApplicationContext
 
 	/**
 	 * Set the ServletContext for this web application context.
-	 * <p>Does not cause an initialization of the context: refresh needs to be
-	 * called after the setting of all configuration properties.
+	 * <p>Does not cause an initialization of the context: refresh needs to be called after the setting of all configuration properties.
+	 * 为此web应用程序上下文设置ServletContext。 不会导致上下文初始化：在设置所有配置属性后需要调用刷新
+	 *
 	 * @see #refresh()
 	 */
 	void setServletContext(@Nullable ServletContext servletContext);
@@ -63,25 +66,31 @@ public interface ConfigurableWebApplicationContext extends WebApplicationContext
 	/**
 	 * Set the ServletConfig for this web application context.
 	 * Only called for a WebApplicationContext that belongs to a specific Servlet.
+	 * 为此web应用程序上下文设置ServletContext。 不会导致上下文初始化：在设置所有配置属性后需要调用刷新
+	 *
 	 * @see #refresh()
 	 */
 	void setServletConfig(@Nullable ServletConfig servletConfig);
 
 	/**
 	 * Return the ServletConfig for this web application context, if any.
+	 * 返回此web应用程序上下文的ServletConfig（如果有）。
+	 *
 	 */
 	@Nullable
 	ServletConfig getServletConfig();
 
 	/**
-	 * Set the namespace for this web application context,
-	 * to be used for building a default context config location.
+	 * Set the namespace for this web application context,to be used for building a default context config location.
 	 * The root web application context does not have a namespace.
+	 * 设置此web应用程序上下文的命名空间，以用于构建默认上下文配置位置。根web应用程序上下文没有命名空间
+	 *
 	 */
 	void setNamespace(@Nullable String namespace);
 
 	/**
 	 * Return the namespace for this web application context, if any.
+	 * 返回此web应用程序上下文的命名空间（如果有）
 	 */
 	@Nullable
 	String getNamespace();
@@ -89,21 +98,22 @@ public interface ConfigurableWebApplicationContext extends WebApplicationContext
 	/**
 	 * Set the config locations for this web application context in init-param style,
 	 * i.e. with distinct locations separated by commas, semicolons or whitespace.
-	 * <p>If not set, the implementation is supposed to use a default for the
-	 * given namespace or the root web application context, as appropriate.
+	 * <p>If not set, the implementation is supposed to use a default for the given namespace or the root web application context, as appropriate.
+	 * 以init param样式设置此web应用程序上下文的配置位置，即使用逗号、分号或空格分隔的不同位置。
+	 * 如果未设置，则实现应该使用给定命名空间或根web应用程序上下文的默认值（视情况而定）
 	 */
 	void setConfigLocation(String configLocation);
 
 	/**
 	 * Set the config locations for this web application context.
-	 * <p>If not set, the implementation is supposed to use a default for the
-	 * given namespace or the root web application context, as appropriate.
+	 * <p>If not set, the implementation is supposed to use a default for the given namespace or the root web application context, as appropriate.
+	 * 设置此web应用程序上下文的配置位置。 如果未设置，则实现应该使用给定命名空间或根web应用程序上下文的默认值（视情况而定）。
 	 */
 	void setConfigLocations(String... configLocations);
 
 	/**
-	 * Return the config locations for this web application context,
-	 * or {@code null} if none specified.
+	 * Return the config locations for this web application context,or {@code null} if none specified.
+	 * 返回此web应用程序上下文的配置位置，如果未指定，则返回null
 	 */
 	@Nullable
 	String[] getConfigLocations();
