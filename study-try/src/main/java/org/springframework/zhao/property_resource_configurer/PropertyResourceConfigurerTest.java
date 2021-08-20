6f6a5757198909cee20ce48a3d1b8ed35e2f0239
RyzeZhao <643080112@qq.com>
2021-08-20 18:24:37 +0800

    Demo::PropertyResourceConfigurer

--- study-try/src/main/java/org/springframework/zhao/property_resource_configurer/PropertyResourceConfigurerTest.java
+++ study-try/src/main/java/org/springframework/zhao/property_resource_configurer/PropertyResourceConfigurerTest.java
@@ -1,14 +1,14 @@
-package org.springframework.zhao.demo;
+package org.springframework.zhao.property_resource_configurer;
 
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
-public class BeanFactoryPostProcessorTest {
+public class PropertyResourceConfigurerTest {
 	public static void main(String[] args) {
 		// 用于测试 PropertyOverrideConfigurer ，通过${}注入值
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/demo/test1/SpringConfig.xml");
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/property_resource_configurer/test1/SpringConfig.xml");
 
 		Student student = applicationContext.getBean(Student.class);
 		System.out.println("student name:" + student.getName() + "-- age:" + student.getAge() + "-- properties:" + student.getProperties());
--- study-try/src/main/java/org/springframework/zhao/property_resource_configurer/Student.java
+++ study-try/src/main/java/org/springframework/zhao/property_resource_configurer/Student.java
@@ -1,9 +1,4 @@
-package org.springframework.zhao.demo;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
+package org.springframework.zhao.property_resource_configurer;
 
 
 public class Student {
