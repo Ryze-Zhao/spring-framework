3015caaac99776ad1cc2799bfee6ff48eb556ac2
RyzeZhao <643080112@qq.com>
2021-10-25 16:57:14 +0800

    Demo::ApplicationContext

--- study-try/src/main/java/org/springframework/zhao/start/config/ZhaoConfig.java
+++ study-try/src/main/java/org/springframework/zhao/start/config/ZhaoConfig.java
@@ -4,6 +4,6 @@ package org.springframework.zhao.start.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan("org.springframework.zhao.start")
+//@Configuration
+//@ComponentScan("org.springframework.zhao.start")
 public class ZhaoConfig {}
