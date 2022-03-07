4f62ef861242142bdaf5c8a24c6b31da326495a3
RyzeZhao <643080112@qq.com>
2022-03-07 18:23:57 +0800

    Demo::Spring MVC

---
+++ study-try-mvc/src/main/java/org/springframework/zhao/pure_interface/WebConfig.java
@@ -0,0 +1,28 @@
+package org.springframework.zhao.pure_interface;
+
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.springframework.zhao.pure_interface")
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+//		registry.jsp("/page/",".html");
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		// 配置消息转换器 这里用fastjson，可以自由选择
+		FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+		converters.add(fastJsonHttpMessageConverter);
+	}
+}
